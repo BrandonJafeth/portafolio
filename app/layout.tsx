6a39f305c2c87dfa47ddc742ebee8678b86dde02
BrandonJafeth <brandon.carrillo.alvarez@est.una.ac.cr>
2024-12-11 17:51:41 -0600

    feat/Imagen del sitio

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+          <head>
+        <link rel="icon" href="/sun-svgrepo-com.svg" type="image/png" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}
       >
