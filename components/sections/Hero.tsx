fbbb038063f3546c649f22819671cc4e3f76fdbd
BrandonJafeth <brandon.carrillo.alvarez@est.una.ac.cr>
2024-12-11 16:22:04 -0600

    feat/ animaciones de fade in

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -10,14 +10,18 @@ import { ScrollIndicator } from "@/components/ui/ScrollIndicator";
 export default function Hero() {
   const { mounted, isDark, toggleTheme } = useThemeToggle();
 
-  if (!mounted) return <div className="h-[80vh]"></div>; // Espacio fijo al cargar
+  if (!mounted) return <div className="h-[80vh]"></div>;
 
   return (
-    <section className="flex flex-col sm:flex-row items-center justify-center min-h-[80vh] p-4 gap-8 relative">
-      {/* Contenedor con tamaño reservado */}
+    <motion.section
+      className="flex flex-col sm:flex-row items-center justify-center min-h-[80vh] p-4 gap-8 relative"
+      initial={{ opacity: 0, y: 50 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+      transition={{ duration: 0.8, ease: "easeOut" }}
+    >
       <div className="flex items-center justify-center relative">
         <div className="relative w-[250px] h-[250px] sm:w-[400px] sm:h-[400px]">
-          {/* Imagen con espacio fijo */}
           <Image
             src="/Main vector.png"
             alt="main"
@@ -28,7 +32,6 @@ export default function Hero() {
           />
         </div>
 
-    
         <motion.div
           className="absolute top-0 right-0 cursor-pointer"
           variants={isDark ? moonVariants : sunVariants}
@@ -38,7 +41,6 @@ export default function Hero() {
           aria-label="Toggle theme"
         >
           {isDark ? (
-           
             <svg
               width="40"
               height="40"
@@ -48,11 +50,10 @@ export default function Hero() {
             >
               <path
                 d="M21 12.79A9 9 0 1111.21 3 7 7 0 0021 12.79z"
-                fill="#ffffff80" 
+                fill="#ffffff80"
               />
             </svg>
           ) : (
-     
             <Image
               src="/sun-svgrepo-com.svg"
               alt="Sun Icon"
@@ -65,7 +66,6 @@ export default function Hero() {
         </motion.div>
       </div>
 
-
       <div className="text-center sm:text-left space-y-4 max-w-sm">
         <h1 className="text-3xl font-bold">
           Hi, I&#39;m Brandon {isDark ? "✨" : "🍃"}
@@ -73,10 +73,9 @@ export default function Hero() {
         <p className="text-md text-foreground/80">{aboutData.introduction}</p>
       </div>
 
-
       <div className="absolute bottom-10 flex justify-center w-full z-10">
         <ScrollIndicator className="text-foreground/80 sm:bottom-4 bottom-6" />
       </div>
-    </section>
+    </motion.section>
   );
 }
