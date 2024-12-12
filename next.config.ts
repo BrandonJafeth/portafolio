8eae8e33d51a407923d87fed45b61288f86aaf4d
BrandonJafeth <brandon.carrillo.alvarez@est.una.ac.cr>
2024-12-12 15:22:03 -0600

    feat /cambio

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,10 @@
 import type { NextConfig } from 'next';
+import siteConfig from './config/site.config';
 
 const nextConfig: NextConfig = {
+  ...siteConfig,
   reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'portfolio-gold-two-44.vercel.app',
-        pathname: '/**',
-      },
-    ],
-    deviceSizes: [320, 420, 768, 1024, 1200],
-    imageSizes: [16, 32, 48, 64, 96],
-    formats: ['image/avif', 'image/webp'],
-  },
-  experimental: {
-    scrollRestoration: true,
-    optimizeCss: true,
-  },
+  swcMinify: true,
 };
 
 export default nextConfig;
